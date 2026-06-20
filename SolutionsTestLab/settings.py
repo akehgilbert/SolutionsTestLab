8bef4c5a9ef416f8db4170a78834f5c1a60543da
Gilbert Akeh <akehgilbert@yahoo.com>
2026-06-21 01:25:08 +0200

    Temporarily use console email backend

--- SolutionsTestLab/settings.py
+++ SolutionsTestLab/settings.py
@@ -100,28 +100,23 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 10
+# =========================
+# TEMPORARY EMAIL SETTINGS
+# =========================
+# This prevents Render worker timeout while testing the contact form.
+# Emails will appear in Render logs instead of being sent by Gmail.
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 EMAIL_HOST_USER = os.environ.get(
     "EMAIL_HOST_USER",
     "akehgilbert2017@gmail.com"
-)
-
-EMAIL_HOST_PASSWORD = os.environ.get(
-    "EMAIL_HOST_PASSWORD",
-    ""
 )
 
 DEFAULT_FROM_EMAIL = os.environ.get(
