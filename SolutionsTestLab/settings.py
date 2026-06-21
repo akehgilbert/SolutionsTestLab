78ba68992de1d248ba8e8af69b895dd389333d35
Gilbert Akeh <akehgilbert@yahoo.com>
2026-06-21 03:13:00 +0200

    Use Namecheap SMTP SSL

--- SolutionsTestLab/settings.py
+++ SolutionsTestLab/settings.py
@@ -113,9 +113,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "mail.privateemail.com"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_PORT = 587
+EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 20
 
 EMAIL_HOST_USER = os.environ.get(
