d3b28096d610d71c022d2e2539455afee7675c3c
Gilbert Akeh <akehgilbert@yahoo.com>
2026-07-01 20:52:02 +0200

    Switch Zoho SMTP to SSL port

--- SolutionsTestLab/settings.py
+++ SolutionsTestLab/settings.py
@@ -131,9 +131,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "smtp.zoho.eu"
-EMAIL_PORT = 587
-EMAIL_USE_SSL = False
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_TIMEOUT = 10
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "").strip()
